edfc0d1cc569fabd775438f15603235e49028fe9
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-10-27 15:43:23 +0700

    mengubah file

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
   CACHE_NAME: 'RestaurantCatalogue-V1',
-  DATABASE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
 };
