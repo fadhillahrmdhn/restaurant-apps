60adda1199a5c4643b846d097eefdb8fb87c12f9
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-10-27 20:05:47 +0700

    mengubah file liking_restaurant_2

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,20 +1,28 @@
+const assert = require('assert');
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
   I.amOnPage('/#/like');
 });
 
-Scenario('liking one Restaurant', ({ I }) => {
+Scenario('liking one Restaurant', async ({I}) => {
   I.amOnPage('/');
-  
-  I.waitForElement('.post-item', 10);
-  I.seeElement('.post-item h2');
-  I.click(locate('.post-item h2').first());
+
+  I.waitForElement('.post-item__title', 10);
+  I.seeElement('.post-item__title a');
+
+  const firstRestaurant = locate('.post-item__title a').first();
+  const firstRestaurantTitle = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
 
   I.waitForElement('#likeButton');
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
+  I.waitForElement('.post-item', 10);
   I.seeElement('.post-item');
+  const likedRestaurantTitle = await I.grabTextFrom('.post-item__title');
+
+  assert.strictEqual(firstRestaurantTitle, likedRestaurantTitle);
 });
