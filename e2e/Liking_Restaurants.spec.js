3c45b56cf89adf07f5133a46856fbbf71df93f51
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2022-10-27 18:19:05 +0700

    mengubah file liking_restaurant

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -7,9 +7,9 @@ Before(({ I }) => {
 Scenario('liking one Restaurant', ({ I }) => {
   I.amOnPage('/');
   
-  I.waitForElement('.post-item__title', 10);
-  I.seeElement('.post-item__title a');
-  I.click(locate('.post-item__title a').first());
+  I.waitForElement('.post-item', 10);
+  I.seeElement('.post-item h2');
+  I.click(locate('.post-item h2').first());
 
   I.waitForElement('#likeButton');
   I.seeElement('#likeButton');
